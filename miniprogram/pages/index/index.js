c1f926d2904bba6eb51549f1b00903cf913bd9b5
shanyudong <1427060889@qq.com>
2024-01-15 17:41:40 +0800

    1.0.2
    经纬度转位置

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,20 +1,43 @@
 // index.js
 
+const key = '6b286c0f26f041c9863ccffd6921cbf4'
+
 Page({
   data: {
     
   },
-  
-  onLoad: function(){
-    //调用wx.getLocation(Object object)，获取用户定位信息（经纬度）
+
+  onLoad: function(){//初始化
+    this.getUserLocation();
+  },
+
+  //获取用户的位置信息
+  getUserLocation: function(){
     wx.getLocation({
       type: 'wgs84',
       success (res) {
-        const latitude = res.latitude;
         const longitude = res.longitude;
-        console.log(latitude,longitude);
+        const latitude = res.latitude;
+        console.log(longitude);
+        wx.request({
+          url: 'https://geoapi.qweather.com/v2/city/lookup',
+          data: {
+            key: key,
+            location: longitude + ',' + latitude
+          },
+          success: function (res) {
+            console.log(res.data)
+          },
+          fail: function (res) {
+            console.log('获取地理位置失败')
+          }
+        })
+      },
+      fail (res) {
+        console.log('获取地理位置失败');
       }
-     })
+     }) 
+
   },
-  
+
 })
