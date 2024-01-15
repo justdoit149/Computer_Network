00b22320655a4337d7ac3b98c462129d6e2b59fa
shanyudong <1427060889@qq.com>
2024-01-15 11:23:48 +0800

    1.0.1
    基本功能-获取定位信息

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,7 +6,15 @@ Page({
   },
   
   onLoad: function(){
-
+    //调用wx.getLocation(Object object)，获取用户定位信息（经纬度）
+    wx.getLocation({
+      type: 'wgs84',
+      success (res) {
+        const latitude = res.latitude;
+        const longitude = res.longitude;
+        console.log(latitude,longitude);
+      }
+     })
   },
   
 })
