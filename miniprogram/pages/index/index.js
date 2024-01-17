7c5966008fdf5a69943948ae64af2cc1beee5974
shanyudong <1427060889@qq.com>
2024-01-17 11:14:05 +0800

    1.0.3
    学习过程中的一些示例代码

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,7 +4,7 @@ const key = '6b286c0f26f041c9863ccffd6921cbf4'
 
 Page({
   data: {
-    
+    msg: "测试信息"
   },
 
   onLoad: function(){//初始化
@@ -19,25 +19,30 @@ Page({
         const longitude = res.longitude;
         const latitude = res.latitude;
         console.log(longitude);
-        wx.request({
-          url: 'https://geoapi.qweather.com/v2/city/lookup',
-          data: {
-            key: key,
-            location: longitude + ',' + latitude
-          },
-          success: function (res) {
-            console.log(res.data)
-          },
-          fail: function (res) {
-            console.log('获取地理位置失败')
-          }
-        })
+        // wx.request({
+        //   url: 'https://geoapi.qweather.com/v2/city/lookup',
+        //   data: {
+        //     key: key,
+        //     location: longitude + ',' + latitude
+        //   },
+        //   success: function (res) {
+        //     console.log(res.data.location[0].name,res.data.location[0].adm1,res.data.location[0].adm2)
+        //   },
+        //   fail: function (err) {
+        //     console.log('获取地理位置失败',err)
+        //   }
+        // })
       },
-      fail (res) {
-        console.log('获取地理位置失败');
+      fail (err) {
+        console.log('获取地理位置失败',err);
       }
      }) 
 
   },
+  onClick: function(){
+    this.setData({
+      msg: "新信息"
+    })
+  }
 
 })
