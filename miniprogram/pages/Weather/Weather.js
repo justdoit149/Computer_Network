dfcc284f2813409101e4e26a628831233bd3f5dd
shanyudong <1427060889@qq.com>
2024-01-17 22:42:48 +0800

    1.1.3
    调用实时天气API

--- miniprogram/pages/Weather/Weather.js
+++ miniprogram/pages/Weather/Weather.js
@@ -1,6 +1,9 @@
 // pages/Weather/Weather.js
 // 天气页，包括Location、Star、NowPart、DayPart、WeekPart、WeatherIndices、BottomNavigation组件
 
+var Util = require('../../util');
+
+
 Page({
 
   /**
@@ -8,14 +11,19 @@ Page({
    */
   data: {
     Location: null,
+    nowWeather: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    //搜索、列表等页面直接切到该城市时，url里也要有Location字段参数！
+    //url传对象参数，需要用JSON.stringify()转成字符串，再用JSON.parse()转回对象！
     this.Location = JSON.parse(options.Location),
-    console.log(this.Location)
+    this.nowWeather = Util.getNowWeather(this.Location.location[0].id)
+    
+    console.log(this.nowWeather)
     
   },
 
--- miniprogram/util.js
+++ miniprogram/util.js
@@ -1,9 +1,22 @@
 module.exports = {
   getUserLocation: getUserLocation, // 获取当前位置信息
-  syncRequest: syncRequest // syncRequest()是wx.request()封装后的异步函数，参数和返回值与wx.request一样
+  syncRequest: syncRequest, // syncRequest()是wx.request()封装后的异步函数，参数和返回值与wx.request一样
+  getNowWeather: getNowWeather
 }
 
 const key = '6b286c0f26f041c9863ccffd6921cbf4'
+
+function getNowWeather(locationID){
+   var res = null
+   res = syncRequest({
+    url: 'https://devapi.qweather.com/v7/weather/now',
+    data: {
+      key: key,
+      location: locationID
+    },
+  })
+  return res
+}
 
 async function getUserLocation(){
   var res = null, res2 = null
@@ -23,12 +36,13 @@ async function getUserLocation(){
         location: res.longitude + ',' + res.latitude
       },
     })
+    console.log(res2)
   } catch (err) {
     console.log('位置请求失败',err)
     return null
   }
-  console.log(res2.data.location[0].name,res2.data.location[0].adm1,res2.data.location[0].adm2)
-  return res2.data;
+  // console.log(res2.data.location[0].name,res2.data.location[0].adm1,res2.data.location[0].adm2)
+  return res2.data
 }
 
 //这段代码以后大概还能用得上
